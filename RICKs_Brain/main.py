a62a4b41eb65328a95f9c65079bc635f9b9a2a6c
Ryan Hiatt <rhiatt614@gmail.com>
2020-11-18 11:04:37 -0600

    Very minor setup

---
+++ RICKs_Brain/main.py
@@ -0,0 +1,17 @@
+import kivy
+
+# Current Version of Kivy
+kivy.require('1.11.1')
+
+from kivy.core import window
+from kivy.app import App
+from kivy.base import runTouchApp
+from kivy.uix.screenmanager import ScreenManager
+
+# ----------------------------------------------------------------------------------------------------------------------
+#   Project: RICK
+#   Description: Radically Intelligent Cocktail Kiosk
+#   Version: 0.2.0
+#   Created By: Ryan Hiatt
+#   Date Started: 11/17/2020
+# ----------------------------------------------------------------------------------------------------------------------
