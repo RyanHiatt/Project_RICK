f07aa6810a170db5e4d9cec4b7d6f0935520819c
Ryan Hiatt <48740827+RyanHiatt@users.noreply.github.com>
2020-12-06 20:54:34 -0600

    Restructured to .kv focus. Finished Login and All Drink Screen basic layout and mechanics.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Project_RICK
  Radically Intelligent Cocktail Kiosk
+
+
+
+pip install --upgrade pip wheel setuptools
+pip install docutils pygments pypiwin32 kivy-deps.sdl2 kivy-deps.glew 
+pip install kivy-deps.gstreamer 
+pip install kivy-deps.angle 
+pip install –-upgrade kivy
--- RICKs_Brain/main.py
+++ RICKs_Brain/main.py
@@ -1,4 +1,6 @@
 import kivy
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -7,8 +9,7 @@ from kivy.uix.popup import Popup
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput
-from kivy.uix.scrollview import ScrollView
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 #   Project: RICK
@@ -25,121 +26,119 @@ kivy.require('1.11.1')
 Window.size = (800, 480)
 # Window.fullscreen = True
 
+PASS_KEY = '8093'
+
 
 class LoginScreen(Screen):
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
+    """
+    -- Description --
+    This screen contains a keypad and a text input. The user will have to input the correct sequence of numerical
+    values and press submit to access the rest of the app. There is also a clear button and an error message popup
+    for clearing wrong inputs and warning users that they input the wrong sequence respectively.
 
-        """
-        -- Description --
-        This screen contains a keypad and a text input. The user will have to input the correct sequence of numerical
-        values and press submit to access the rest of the app. There is also a clear button and an error message popup
-        for clearing wrong inputs and warning users that they input the wrong sequence respectively.
-        
-        -- Example --
-        
-        
-        -- Structure --
+    -- Usage --
+    Access Bar Application
+
+    -- Structure --
+    GridLayout:
+        Label:
         GridLayout:
             Label:
+            TextInput:
             GridLayout:
-                Label:
-                TextInput:
-                GridLayout:
-                    Button(x12):
-                Label:
+                Button(x12):
             Label:
-        """
+        Label:
+    """
 
-        # Main Grid
-        main_grid = GridLayout(cols=3)
+    current_key = ''
 
-        # Left Column
-        label1 = Label()
-        main_grid.add_widget(label1)
-
-        # Center Column
-        center_grid = GridLayout(rows=4)
-        label3 = Label(size_hint_y=0.1)
-        center_grid.add_widget(label3)
-
-        # Row 1
-        text = TextInput(size_hint_y=0.2)
-        center_grid.add_widget(text)
-
-        # Row 2
-        key_grid = GridLayout(cols=3, rows=4)
-        center_grid.add_widget(key_grid)
-
-        # Key Grid
-        for i in range(1, 10):
-            btn = Button(text=str(i),
-                         font_size=80,
-                         id='btn' + str(i),
-                         on_release=self.btn_press)
-            key_grid.add_widget(btn)
-
-        key_grid.add_widget(Button(text='Clear',
-                                   font_size=50,
-                                   id='clear',
-                                   on_release=self.btn_press))
-        key_grid.add_widget(Button(text='0',
-                                   font_size=80,
-                                   id='btn0',
-                                   on_release=self.btn_press))
-        key_grid.add_widget(Button(text='Submit',
-                                   font_size=50,
-                                   id='submit',
-                                   on_release=self.submit))
-
-        # Row 3
-        label4 = Label(size_hint_y=0.1)
-        center_grid.add_widget(label4)
-
-        # Add Center Grid to Main Grid
-        main_grid.add_widget(center_grid)
-
-        # Right Column
-        label2 = Label()
-        main_grid.add_widget(label2)
-
-        # Load Screen
-        self.add_widget(main_grid)
-
+    # Runs every time a numerical button is pressed
     def btn_press(self, instance):
-        pass
+        LoginScreen.current_key += str(instance.text)
+        self.ids.passkey.text = self.ids.passkey.text + '*'
 
-    def clear_values(self):
-        pass
+    # Runs when clear button is pressed (clears input)
+    def clear_text(self):
+        self.ids.passkey.text = ''
+        LoginScreen.current_key = ''
 
-    def submit(self, *args):
-        self.manager.current = 'home'
+    # Runs when submit button is pressed, checks for correct key
+    def submit(self):
+        if LoginScreen.current_key == PASS_KEY:
+            self.ids.passkey.text = ''
+            LoginScreen.current_key = ''
+            self.manager.current = 'home'
+
+        else:
+            self.ids.passkey.text = ''
+            LoginScreen.current_key = ''
+
+            # Creates Incorrect Passkey Popup
+            popup = Popup(title='Incorrect Passkey',
+                          title_align='center',
+                          size_hint=(0.5, 0.5),
+                          auto_dismiss=False)
+
+            # Popup Layout
+            pop_layout = GridLayout(rows=2)
+
+            # Popup Label
+            nope_label = Label(text='Incorrect Passkey\n Please Try Again',
+                               font_size=40)
+            pop_layout.add_widget(nope_label)
+
+            # Popup Close Button
+            close_btn = Button(text='Close',
+                               size_hint_y=0.4)
+            close_btn.bind(on_release=popup.dismiss)
+            pop_layout.add_widget(close_btn)
+
+            # Add Layout to Popup
+            popup.add_widget(pop_layout)
+
+            # Open Popup
+            popup.open()
 
 
 class HomeScreen(Screen):
     """
     -- Description --
-    The home screen as it implies is the central navigation hub of the whole application. From here
+    The home screen as it implies is the central navigation hub of the whole application. From here users can navigate
+    through the rest of the application through the use of button or log out back to the login screen.
+
+    -- Usage --
+    Access All Drinks Screen
+    Access Shots Screen
+    Access Favorites Screen
+    Access Create Drink Screen
+    Access Settings Screen
+    Revert to Login Screen
 
     -- Structure --
-
+    GridLayout:
+        GridLayout:
+            Label:
+            Label:
+            Label:
+        GridLayout:
+            AnchorLayout:
+                Button:
+            AnchorLayout:
+                Button:
+            AnchorLayout:
+                Button:
+            AnchorLayout:
+                Button:
+        GridLayout:
+            AnchorLayout:
+                Button:
+            Label:
+            AnchorLayout:
+                Button:
     """
 
     def open_lockpop(self):
-        """
-        -- Description --
-
-
-        -- Structure --
-        Popup:
-            GridLayout:
-                Label:
-                GridLayout:
-                    AnchorLayout:
-                        Button:
-                    AnchorLayout:
-                        Button:
-        """
 
         # Initiate Popup
         pop = Popup(size_hint=(0.5, 0.5),
@@ -162,7 +161,7 @@ class HomeScreen(Screen):
 
         # Row 2
         btn_grid = GridLayout(size_hint_y=0.33,
-                           cols=2)
+                              cols=2)
 
         # No Button
         anchor1 = AnchorLayout(anchor_x='left',
@@ -173,8 +172,8 @@ class HomeScreen(Screen):
                       font_size=20,
                       on_release=pop.dismiss)
         anchor1.add_widget(btn1)
-        btn_grid.add_widget(anchor1)
 
+        btn_grid.add_widget(anchor1)
         # Yes Button
         anchor2 = AnchorLayout(anchor_x='right',
                                anchor_y='bottom')
@@ -201,7 +200,14 @@ class HomeScreen(Screen):
 class AllDrinksScreen(Screen):
     """
     -- Description --
+    This screen contains all the drinks available in the bar app. They will be organized alphabetically and displayed in
+    a scrolling manor. The user can either go back to the home screen, select a drink, or select the categories button
+    to be brought to the Categories Screen in which drinks are broken down into categories.
 
+    -- Usage --
+    Access Categories Screen
+    Dispense Drink
+    Revert to Home Screen
 
     -- Structure --
     GridLayout:
@@ -216,77 +222,47 @@ class AllDrinksScreen(Screen):
                 Button:
     """
 
+    drink_layout = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(AllDrinksScreen, self).__init__(**kwargs)
 
-        # Main Grid
-        main_grid = GridLayout(rows=2)
+        Clock.schedule_once(self.setup_scrollview, 1)
 
-        # Scroll View
-        scroll = ScrollView(do_scroll_x=False,
-                            bar_width=10,
-                            bar_color=(0, 0, 1, 0.5),
-                            bar_inactive_color=(0, 0, 1, 1),
-                            effect_cls='DampedScrollEffect')
-        # Button Grid
-        btn_grid = GridLayout(cols=4)
+    def setup_scrollview(self, dt):
+        self.drink_layout.bind(minimum_height=self.drink_layout.setter('height'))
+        self.create_drink_btns()
 
-        # Buttons
+    def create_drink_btns(self):
         for i in range(100):
-            btn_grid.add_widget(Button(text=str(i),
-                                       font_size=40))
+            self.drink_layout.add_widget(Button(text='Button {}'.format(i+1),
+                                                size_hint_y=None,
+                                                size_hint_x=None,
+                                                height=180,
+                                                width=220))
 
-        scroll.add_widget(btn_grid)
 
-        # Bottom Row
-        btm_grid = GridLayout(cols=3,
-                              size_hint_y=0.25)
+class DispenseScreen(Screen):
+    """
+    -- Description --
 
-        # Back Button
-        anchor1 = AnchorLayout(anchor_x='left',
-                               anchor_y='bottom',
-                               size_hint_x=0.75,
-                               padding=(8, 10, 5, 5))
 
-        anchor1.add_widget(Button(text='Back',
-                                  font_size=20,
-                                  on_release=self.back))
+    -- Usage --
 
-        btm_grid.add_widget(anchor1)
 
-        # RICK Label
-        label = Label(text='R.I.C.K.',
-                      font_size = 60,
-                      halign='center',
-                      valign='middle')
-        btm_grid.add_widget(label)
+    -- Structure --
 
-        # Categories Button
-        anchor2 = AnchorLayout(anchor_x='left',
-                               anchor_y='bottom',
-                               size_hint_x=0.75,
-                               padding=(8, 10, 5, 5))
+    """
 
-        anchor2.add_widget(Button(text='Categories',
-                                  font_size=20,
-                                  on_release=self.categories))
-
-        btm_grid.add_widget(anchor2)
-        main_grid.add_widget(scroll)
-        main_grid.add_widget(btm_grid)
-
-        self.add_widget(main_grid)
-
-    def back(self, *args):
-        self.manager.current = 'home'
-
-    def categories(self, *args):
-        self.manager.current = 'category'
+    pass
 
 
 class CategoriesScreen(Screen):
     """
     -- Description --
+
+
+    -- Usage --
 
 
     -- Structure --
@@ -301,6 +277,9 @@ class ShotsScreen(Screen):
     -- Description --
 
 
+    -- Usage --
+
+
     -- Structure --
 
     """
@@ -311,6 +290,9 @@ class ShotsScreen(Screen):
 class FavoritesScreen(Screen):
     """
     -- Description --
+
+
+    -- Usage --
 
 
     -- Structure --
@@ -325,6 +307,9 @@ class CreateDrinkScreen(Screen):
     -- Description --
 
 
+    -- Usage --
+
+
     -- Structure --
 
     """
@@ -335,10 +320,19 @@ class CreateDrinkScreen(Screen):
 class MyScreenManager(ScreenManager):
     """
     -- Description --
-
+    This object manages all the screens for the application as well as screen transitions.
 
     -- Structure --
-
+    LoginScreen:
+        HomeScreen:
+            AllDrinksScreen:
+                DispenseScreen:
+                CategoriesScreen:
+            ShotsScreen:
+                DispenseScreen:
+            FavoritesScreen:
+                DispenseScreen:
+            CreateDrinkScreen:
     """
 
     def __init__(self, **kwargs):
@@ -347,8 +341,10 @@ class MyScreenManager(ScreenManager):
         screens = [LoginScreen(name='login'),
                    HomeScreen(name='home'),
                    AllDrinksScreen(name='drinks'),
+                   DispenseScreen(name='dispense'),
                    CategoriesScreen(name='category'),
                    ShotsScreen(name='shots'),
+                   FavoritesScreen(name='favorites'),
                    CreateDrinkScreen(name='create')]
 
         for screen in screens:
